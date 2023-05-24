ff40d86124a0afea9f84e722af02c3ff506ce2b1
Matheus Marques <matheusmdev97@outlook.com>
2023-05-24 18:54:19 -0300

    [style] time

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { PomodoroTimer } from './components/pomodoro-timer';
 function App(): JSX.Element {
   return (
     <div className="App">
-     <PomodoroTimer PomodoroTime={3600} shortRestTime={300} longRestTime={900} cycles={4}/>
+     <PomodoroTimer PomodoroTime={1800} shortRestTime={300} longRestTime={900} cycles={4}/>
     </div>
   );
 }
